73987d5d31fd224bec28ed0d48c2c4b821d47ab3
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-01 16:44:35 -0500

    Selenium methods quite and close

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -19,6 +19,9 @@ public class BasicNavigations {
         //this line will maximize the browser size
         driver.manage().window().maximize();
 
+        //this is mac functionality
+        //driver.manage().window().fullscreen();
+
         //3- Go to "https://www.tesla.com"
         driver.get("https://www.tesla.com");
 
@@ -65,9 +68,11 @@ public class BasicNavigations {
         currentURL = driver.getCurrentUrl();
         System.out.println("currentURL = " + currentURL);
 
+        //this will close the currently opened window
+        driver.close();
 
-
-
+        //this will close all of the opened windows
+        //driver.quit();
 
     }
 }
