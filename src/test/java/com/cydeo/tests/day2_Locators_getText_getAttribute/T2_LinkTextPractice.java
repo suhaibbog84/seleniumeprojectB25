273a335842e0054152f0a6246e11ac25ed71ec6f
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-04 11:34:53 -0500

    Day2 Selenium - T2_LinkText_practice

---
+++ src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/T2_LinkTextPractice.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day2_Locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkTextPractice {
+
+    public static void main(String[] args) {
+
+        //TC #3: Back and forth navigation
+        //1- Open a Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.yahoo.com");
+
+        //2- Go to: https://practice.cydeo.com
+        //3- Click to A/B Testing from top of the list.
+        //4- Verify title is:
+        //  Expected: No A/B Test
+        //5- Go back to home page by using the .back();
+        //6- Verify title equals:
+        //  Expected: Practice
+
+
+
+
+    }
+}
