136618c3c0c33267d2baa0bbf7cafc3780ba9fb0
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-04 10:00:30 -0500

    Day1 Selenium - practice methods

---
+++ src/test/java/SeleniumPractice/Day1_Practice.java
@@ -0,0 +1,45 @@
+package SeleniumPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1_Practice {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.Google.com");
+
+        //Thread.sleep(1500);
+
+        //driver.navigate().back();
+
+        //Thread.sleep(1500);
+
+        //driver.navigate().forward();
+
+        //Thread.sleep(1500);
+
+        //driver.navigate().refresh();
+
+        Thread.sleep(1500);
+
+        String pageTitle = driver.getTitle();
+        System.out.println("pageTitle " + pageTitle);
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println(currentURL);
+
+
+
+        //driver.close(); //to close the current open driver after open it
+
+        driver.quit(); //to close the entire browser
+
+
+    }
+}
