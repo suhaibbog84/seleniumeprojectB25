e016ac466ad64e47b8e605baed5e29fc3cb87c59
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-04 14:40:21 -0500

    Day2 Selenium - T3_Google Search

--- src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_Locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoogleSearch {
@@ -14,9 +17,16 @@ public class T3_GoogleSearch {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        //2- Go to: https://google.com/
+        //2- Go to: https://google.com
+        driver.get("https://google.com");
+
         //3- Write “apple” in search box
+        WebElement googleSearchBox = driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+
         //4- Click google search button
+
+
         //5- Verify title:
         //Expected: Title should start with “apple” word
 
