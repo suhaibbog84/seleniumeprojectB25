bcc69ffefacaef160b2a3a50079e8ec7c6a91d96
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-04 11:52:50 -0500

    Day2 Selenium - T2_LinkText_practice

--- src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/T2_LinkTextPractice.java
+++ src/test/java/com/cydeo/tests/day2_Locators_getText_getAttribute/T2_LinkTextPractice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_Locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T2_LinkTextPractice {
@@ -12,10 +14,16 @@ public class T2_LinkTextPractice {
         //1- Open a Chrome browser
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.yahoo.com");
+        driver.manage().window().maximize();
 
         //2- Go to: https://practice.cydeo.com
+        driver.get("https://practice.cydeo.com");
+
         //3- Click to A/B Testing from top of the list.
+        //driver.findElement(By.linkText("A/B Testing")).click();
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));
+        abTestLink.click();
+
         //4- Verify title is:
         //  Expected: No A/B Test
         //5- Go back to home page by using the .back();
