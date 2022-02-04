d9bb8d299bbfea61b37b2be18b1ce1d93814b332
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-04 09:59:47 -0500

    Day2 Selenium - Elements Locator

---
+++ src/test/java/SeleniumPractice/ElementsLocators_ID.java
@@ -0,0 +1,37 @@
+package SeleniumPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ElementsLocators_ID {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.yahoo.com");
+
+        //identify and sane the element
+
+        WebElement inputBox = driver.findElement(By.id("yschsp"));
+
+        //enter text hello
+        inputBox.sendKeys("Hello");
+
+        WebElement searchBtn = driver.findElement(By.id("yschsp"));
+
+        searchBtn.click();
+
+        Thread.sleep(1500);
+
+        driver.quit();
+
+
+
+    }
+}
