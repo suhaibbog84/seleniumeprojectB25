e5515f075670461a10ac8d4b0c9c9a600a08fd55
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-08 11:45:48 -0500

    Day3 Selenium - T1_Homework

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -33,6 +33,16 @@ public class T1_locators_getText {
 
             //6- Verify error message text is as expected:
             //Expected: Incorrect login or password
+        WebElement errorMessage = driver.findElement(By.className("errortext"));\
+
+        String expectedErrorMessage = "Incorrect login or password";
+        String actualErrorMessage = errorMessage.getText();
+
+        if (actualErrorMessage.equals(expectedErrorMessage)){
+            System.out.println("Error message test passed");
+        }else{
+            System.out.println("Error message test failed");
+        }
 
 
         }
