37e379abf170505260cb2cf153e63ab168f3a8c6
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-08 12:24:21 -0500

    Day3 Selenium - 1st utility class

---
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -0,0 +1,24 @@
+package com.cydeo.utilities;
+
+// TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+
+  public static WebDriver getDriver(String browserType){
+
+  }
+
+
+
+
+}
