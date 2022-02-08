2f884ce6802ec4a59f1fe17ecec1467d575db7cb
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-08 12:32:09 -0500

    Day3 Selenium - 1st utility class

--- src/test/java/com/cydeo/utilities/WebDriverFactory.java
+++ src/test/java/com/cydeo/utilities/WebDriverFactory.java
@@ -9,16 +9,28 @@ package com.cydeo.utilities;
 //   - if "firefox" passed --> it will open firefox browser
 // RETURN TYPE: "WebDriver"
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
 
-  public static WebDriver getDriver(String browserType){
+  public static WebDriver getDriver(String browserType) {
+
+    if (browserType.equalsIgnoreCase("chrome")) {
+
+      WebDriverManager.chromedriver().setup();
+      return new ChromeDriver();
+    }else if(browserType.equalsIgnoreCase("safari")){
+      WebDriverManager.safaridriver().setup();
+      return new SafariDriver();
+    }else {
+      System.out.println("Given browser type does not exist/or is not currently supported");
+      System.out.println("Driver = null");
+      return null;
+    }
+
 
   }
-
-
-
-
 }
