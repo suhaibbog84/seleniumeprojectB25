424cb544b56ad8b8fa6b58e6ec4e5b774b6abe71
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-11 14:08:40 -0500

    Day4 Selenium - T1 xPath _ cssSelector - home button

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xPath_sssSelector_Practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xPath_sssSelector_Practice.java
@@ -32,9 +32,25 @@ public class T1_xPath_sssSelector_Practice {
 
 
         //b. “Forgot password” header
+        //locate header using cssS: locate parent element and move down to h2
+        WebElement header_ex1 = driver.findElement(By.cssSelector("a[href='/']"));
+
+        //locate header using xPath, and element text
+        WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
+        WebElement header_ex3 = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+
         //c. “E-mail” text
-        //d. E-mail input box
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
+        //d. E-mail input box using xPath by name
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+
+        //locate inputBox using xPath by contains method
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z0-9')]"));
+
         //e. “Retrieve password” button
+        WebElement retrievePasswordBtn = driver.findElement(By.xpath("//button/i[.='Retrieve password']"));
+
         //f. “Powered by Cydeo text
         //4. Verify all web elements are displayed.
         //First solve the task with using cssSelector only. Try to create 2 different
