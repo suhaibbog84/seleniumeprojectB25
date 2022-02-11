616ef9ac78a1b57b6c508dbb4fc096dfb5eebc3a
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-11 15:36:47 -0500

    Day4 Selenium - T4 findElements

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T4_findElements.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T4_findElements.java
@@ -14,10 +14,11 @@ public class T4_findElements {
 
         //TC #4: FindElements Task
         //1- Open a chrome browser
-        WebDriver driver = WebDriverFactory.getDriver("Chrome")
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
         //driver.manage().window().maximize();
 
         //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
 
         //Provide extra wait time for our driver before it throws NoSuchElementException
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -26,9 +27,19 @@ public class T4_findElements {
         List<WebElement> allLinks = driver.findElements(By.tagName("a"));
 
         //4- Print out the number of the links on the page.
-        //5- Print out the texts of the links.
-        //6- Print out the HREF attribute values of the links
+        System.out.println("allLinks.size() = " + allLinks.size());
 
+        //5- Print out the texts of the links.
+        for (WebElement each : allLinks) {
+            System.out.println("each.getText() = " + each.getText());
+        }
+
+        //6- Print out the HREF attribute values of the links
+        for (WebElement each : allLinks) {
+            System.out.println("each.getAttribute() = " + each.getAttribute("href"));
+        }
+
+        driver.quit();
 
 
 
