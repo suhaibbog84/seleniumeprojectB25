403f6d2bfad77a9e167d8fce210c165f50ce9b86
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-12 17:24:09 -0500

    Day4 Selenium - T6 stale Element Reference Exception

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 public class T6_StaleElementReferenceException {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TC #6: StaleElementReferenceException Task
         //1- Open a chrome browser
@@ -25,13 +25,14 @@ public class T6_StaleElementReferenceException {
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
         //4- Refresh the page.
-
-
+        driver.navigate().refresh();
 
         //5- Verify it is displayed, again.
+        cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
-
-
+        Thread.sleep(2000);
+        driver.quit();
 
     }
 }
