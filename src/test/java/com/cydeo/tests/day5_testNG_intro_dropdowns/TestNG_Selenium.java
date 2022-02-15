611f3fba6b43e091461ce921c4d36b5eaf29fe71
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-15 15:37:04 -0500

    Day5 Selenium - seleniumTest

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Selenium {
+
+    @Test
+    public void seleniumTest(){
+        //Do browser driver setup
+        //Open browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        //Maximize the page
+        //driver.manage().window().maximize();
+
+        //Implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Get "https://google.com"
+        driver.get("https://google.com");
+
+        //Assert: title is  "Google"
+
+    }
+
+}
