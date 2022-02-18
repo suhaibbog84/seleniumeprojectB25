620af78669d5f6e7019fe2e33b274c842a6a68d8
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-18 14:40:46 -0500

    Day6 Selenium - Alert Practice

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practice.java
@@ -0,0 +1,54 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        //1. open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to http://practice.cydeo.com/javascript_alerts
+        driver.get("http://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test1() throws InterruptedException {
+
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+        informationAlertButton.click();
+
+        //to be able to click on the alert ok button we need to switch driver's focus to Alert itself
+        Alert alert = driver.switchTo().alert();
+
+        //4. Click to OK button from the alert
+        alert.accept();
+
+        //5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement resultTest = driver.findElement(By.xpath("//p[@id='result']"));
+
+
+
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+
+
+}
