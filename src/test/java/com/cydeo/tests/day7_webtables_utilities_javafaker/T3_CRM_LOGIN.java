deb51389ae7b6b8e587c9c4fc8d6778eee7de09d
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-22 16:27:58 -0500

    T3 CRM Login

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -30,21 +30,7 @@ public class T3_CRM_LOGIN {
         //2. Go to : http://login1.nextbasecrm.com/
         driver.get("http://login1.nextbasecrm.com/");
 
-        //3. Enter valid username
-        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
-        inputUsername.sendKeys("helpdesk1@cybertekschool.com ");
 
-        //USERNAME  PASSWORD
-        //helpdesk1@cybertekschool.com  UserUser
-        //Helpdesk2@cybertekschool.com  UserUser
-
-        //4. Enter valid password
-        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
-        inputPassword.sendKeys("UserUser");
-
-        //5. Click to Log In button
-        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
-        loginButton.click();
 
         //6. Verify title is as expected:
         //Expected: Portal
