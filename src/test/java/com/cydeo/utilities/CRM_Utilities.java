1de2f3f9e6cb82144413a6f079e5a94613c7e4bf
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-22 16:27:48 -0500

    CRM Utilities

---
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -0,0 +1,33 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CRM_Utilities {
+
+    //This method will login with helpdesk1@cybertekschool.com
+    // user when it's called
+
+    public static void crm_login(WebDriver driver){
+
+        //3. Enter valid username
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com ");
+
+        //USERNAME
+        //helpdesk1@cybertekschool.com  UserUser
+        //Helpdesk2@cybertekschool.com  UserUser
+
+        //4. Enter valid password
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to Log In button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+    }
+
+
+}
