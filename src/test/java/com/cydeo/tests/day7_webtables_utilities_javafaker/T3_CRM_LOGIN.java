ce8cef97fd1b3c43e6809555020404df9cc8a3c0
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-22 16:46:41 -0500

    T3 - CRM Login

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -30,12 +30,28 @@ public class T3_CRM_LOGIN {
         driver.get("http://login1.nextbasecrm.com/");
 
         //calling my utility method to login helpdesk1
-        CRM_Utilities.crm_login(driver);
+        CRM_Utilities.crm_login(driver, "Helpdesk1@cybertekschool.com","UserUser");
 
         //6. Verify title is as expected:
         //Expected: Portal
         BrowserUtils.verifyTitle(driver, "Portal");
 
+    }
+
+    @Test
+    public void crm_login_test_3(){
+
+        //2. Go to : http://login1.nextbasecrm.com/
+        driver.get("http://login1.nextbasecrm.com/");
+
+        //calling my utility method to login helpdesk2
+        CRM_Utilities.crm_login(driver, "Helpdesk2@cybertekschool.com", "UserUser");
+
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver, "(2) Portal");
+
+        driver.quit();
     }
 
 
