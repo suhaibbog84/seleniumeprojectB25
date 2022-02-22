c478bb748a01273b7d0bd0e6ce60d90e63290458
Suhaib B <suhaib.bog84@yahoo.com>
2022-02-22 15:11:37 -0500

    Browser Thread.sleep method

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -5,7 +5,7 @@ public class BrowserUtils {
     //this method will accept int (in seconds) and execute Thread.sleep
     //for given duration
 
-    public void sleep (int second){
+    public void sleep(int second){
         second *=1000;
         try {
             Thread.sleep(second);
