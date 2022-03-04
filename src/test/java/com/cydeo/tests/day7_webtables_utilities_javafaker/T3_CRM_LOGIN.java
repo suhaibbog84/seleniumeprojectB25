969c7e950a15fdb2e5866d49c4d1a9dc152a6639
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-04 14:24:28 -0500

    T3 CRM LOGIN

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,28 +1,11 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
-import com.cydeo.utilities.WebDriverFactory;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class T3_CRM_LOGIN {
-
-    public WebDriver driver;
-
-    @BeforeMethod
-    public void setUpMethod(){
-        //TC #3: Login scenario
-        // 1. Create new test and make set ups
-
-        driver = WebDriverFactory.getDriver("chrome");
-        //driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }
+public class T3_CRM_LOGIN extends TestBase {
 
     @Test
     public void crm_login_test(){
