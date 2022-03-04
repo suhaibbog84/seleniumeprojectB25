8e8b6f74068c2043029e213c34aee92004d2d17c
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-04 16:25:45 -0500

    Singleton

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+public class Singleton {
+
+    //#1- create private constructor
+
+    private Singleton(){}
+
+    //2- create private static String
+    // prevent access and provide a getter method
+    private static String word;
+
+    // this utility method will return the "word" in the way
+    //we want to return
+    public static String fetWord(){
+
+        if (word == null){
+            System.out.println("First time call. Word object is null. " +
+                    "Assign value to it now!");
+            word = "somthing";
+        }else{
+            System.out.println("Word already has value");
+        }
+
+        return word;
+
+    }
+
+
+}
