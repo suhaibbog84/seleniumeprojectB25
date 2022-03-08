4a839b7f1922a192959c8413805495541dbe349f
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-08 14:36:24 -0500

    day10 - T2 upload practice

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecuter/T2_Upload_Practice.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.day10_upload_actions_jsexecuter;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class T2_Upload_Practice {
+
+    @Test
+    public void upload_test(){
+
+        //TC #2 Upload Test
+        //1. Go to https://practice.cydeo.com/upload
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+        //2. Find some small file from your computer, and get the path of it.
+        String path = "/Users/suhaibbogerma/Desktop/HTML Class/Razona.HEIC";
+
+        //3. Upload the file.
+        WebElement fileUpload = Driver.getDriver().findElement(By.xpath("//input[@id='file-upload']"));
+        fileUpload.sendKeys(path);
+
+        //4. Assert:
+        //-File uploaded text is displayed on the page
+
+
+    }
+
+
+}
