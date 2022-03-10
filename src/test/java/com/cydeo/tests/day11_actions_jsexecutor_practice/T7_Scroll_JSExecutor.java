ba9fd55a3220e9ec77ecab77f53b2cbca341accd
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-10 13:15:41 -0500

    day11 - T7 JSExecutor - scroll

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -10,20 +10,21 @@ import org.testng.annotations.Test;
 public class T7_Scroll_JSExecutor {
 
     @Test
-    public void task7_scroll_test() {
-        //TC #7: Scroll using JavascriptExecutor
-        //1- Open a chrome browser
+    public void task7_scroll_test(){
+        //1- Open a Chrome browser
         //2- Go to: https://practice.cydeo.com/large
         Driver.getDriver().get("https://practice.cydeo.com/large");
 
         WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
         WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
 
-        JavascriptExecutor js = (JavascriptExecutor)Driver.getDriver();
+        //Down-casting our driver type to JavascriptExecutor,
+        // so we are able to use the methods coming from that interface
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
         //3- Scroll down to “Cydeo” link
         BrowserUtils.sleep(2);
-        js.executeScript(argument[0].scrollIntoView(true), cydeoLink);
+        js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
 
         //4- Scroll up to “Home” link
         BrowserUtils.sleep(2);
