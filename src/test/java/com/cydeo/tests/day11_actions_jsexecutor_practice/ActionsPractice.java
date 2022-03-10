3e6ea57dec13982d095e10d115702f7d60c2eb31
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-10 11:18:27 -0500

    day11 - T4 & T5 actions practice

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractice.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractice.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -30,6 +31,7 @@ public class ActionsPractice {
         //TC #5: Scroll practice 2
         //1- Continue from where the Task 4 is left in the same test.
         //2- Scroll back up to “Home” link using PageUP button
+        actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();
 
 
     }
